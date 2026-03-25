377ce6d350dc285140af99ee3fe150703e71f53e
jay-p08 <soRjdla0709@gmail.com>
2026-03-26 08:28:53 +0900

    [ BK ] A+B - 7 / Bronze V

---
+++ jay-p08/BaekJoon/11021.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N, A, B;
+    cin >> N;
+
+    for( size_t i = 0 ; i < N ; i++ )
+    {
+        cin >> A >> B;
+        cout << "Case #" << i + 1 << ": " << A + B << endl;
+    } // for()
+
+    return 0;
+}
