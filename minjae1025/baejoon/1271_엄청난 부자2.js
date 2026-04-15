d33a2d8bae2325c95eec823d302be791f12a6a13
minjae1025 <kjt081025@gmail.com>
2026-04-15 20:16:29 +0900

    기존 폴더 삭제

--- minjae1025/baejoon/1271_엄청난 부자2.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split(" ");
-const n = BigInt(input[0]);
-const m = BigInt(input[1]);
-
-console.log((n / m).toString());
-console.log((n % m).toString());
