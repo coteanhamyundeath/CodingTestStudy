f51397c95e5ab4a91cb399673402fda440ec5c59
minjae1025 <kjt081025@gmail.com>
2026-04-29 19:47:09 +0900

    2 x n 타일링 / Lv.2

---
+++ minjae1025/programmers/2 x n 타일링.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let number = new Array(n + 1).fill(0);
+    number[0] = 1;
+    number[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        number[i] = (number[i - 1] + number[i - 2]) % 1000000007;
+    }
+
+    return number[n];
+}
+
+// console.log(solution(4));
